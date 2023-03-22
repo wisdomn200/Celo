9707f2021029842c1b2bedc6186b2f61f53d8461
wisdomn200 <wisdomn200@gmail.com>
2023-03-22 14:08:32 +0100

    Graph api built ssuccesfully

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+// Add the alfajores network to the configuration
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC_KEY,
+        path: "m/44'/52752'/0'/0",
+      },
+      chainId: 44787,
+    },
+  },
+};
+
